fd8107e615eadd02cedeb8445b25b9d5c503ea9e
surendrasingh@Surendras-MacBook-Air.local <ss0622187@gmail.com>
2024-07-27 00:11:14 +0530

    done

--- config/database.js
+++ config/database.js
@@ -18,12 +18,17 @@ const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
-const postgresUrl = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const postgresUrl =   'postgresql://invoice_agcu_user:8hSg3ILOEuRDHBGq33gX3aeDzAuAVCIp@dpg-cqhudg9u0jms739dh9s0-a.singapore-postgres.render.com/invoice_agcu' || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 // PostgreSQL connection
 const sequelize = new Sequelize(postgresUrl, {
     logging: console.log,  // This will log SQL queries to the console
     dialect: 'postgres',
-  protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // This option allows self-signed certificates
+      }
+    }
   });
 // Test PostgreSQL connection
 const connectPostgres = async () => {
